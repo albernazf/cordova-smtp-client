e0c999ebacee3200e19f8d31f87a3ba76897c988
Fernando Albernaz <albernazf@outlook.com>
2015-10-11 23:19:40 +1100

    fixed js properties

--- www/MailSettings.ts
+++ www/MailSettings.ts
@@ -5,10 +5,14 @@ class MailSettings {
     public textBody: string;
     public smtp: string;
     public port: number;
-    public smtpUserName: string;
+    public sport: number;
+	public auth: boolean;
+	public ssl: boolean;
+	public smtpUserName: string;
     public smtpPassword: string;
     public smtpRequiresAuth: boolean;
     public attachments: Array<string>;
+	
  
     constructor(mailSettings: any = undefined) {
         if (mailSettings != undefined) {
@@ -16,6 +20,9 @@ class MailSettings {
             this.emailTo = mailSettings.emailTo;
             this.smtp = mailSettings.smtp;
             this.port = mailSettings.port;
+			this.sport = mailSettings.sport;
+			this.auth = mailSettings.auth;
+			this.ssl = mailSettings.ssl;
             this.smtpUserName = mailSettings.smtpUserName;
             this.smtpPassword = mailSettings.smtpPassword;
             this.attachments = mailSettings.attachments;
